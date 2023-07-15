5fbb15c3a9591fc87af283cfd822af6fc6a0329d
Natosphere <nathanwheelwright@gmail.com>
2023-07-14 22:12:30 -0700

    ...

--- create_conda.bat
+++ create_conda.bat
@@ -1,5 +1,5 @@
 set root=C:\Users\Nathan\miniconda3
-call %root%Scripts\activate.bat %root%
+call %root%\Scripts\activate.bat %root%
 
 REM md envs
 call conda env create --prefix=envs --file=environment.yml
