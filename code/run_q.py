e90b15d388f4ce1d082c03d132198e88032c4a85
Natosphere <nathanwheelwright@gmail.com>
2024-03-13 23:12:51 -0700

    Update run_q.py
    updated

--- code/run_q.py
+++ code/run_q.py
@@ -1,11 +1,7 @@
 import gymnasium as gym
-from agents.Q_Agent import QLearningAgent
-from gymnasium.vector import AsyncVectorEnv
-from agents.Q_Agent_Vector import QLearningAgentVector
+from agents.Q_Agent import QAgent
 
 n_episodes = 60000
-
-
 
 
 learning_rate=0.7              # 0 means the agent doesn't update q-table at all. 1 means the agent rewrites entire q-table. 
@@ -17,7 +13,7 @@ exploration_decay_rate=0.0001   # epsilon decay rate
 min_exploration_rate=0.02
 file_save_path="./code/models/"
 num_bins=[30,30,40,20]              # Adjust the number of bins based on your environment. length depends on observation space variables, 
-                                    # the number size correlates to how granular you want the analog measurements to be digitized into.
+									# the number size correlates to how granular you want the analog measurements to be digitized into.
 
 
 
@@ -25,19 +21,19 @@ num_bins=[30,30,40,20]              # Adjust the number of bins based on your en
 env = gym.make('CartPole-v1', render_mode='rgb_array')
 env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
 # Create an instance of the QLearningAgent
-agent = QLearningAgent(
-    env, 
-    learning_rate,
-    learning_decay_rate,
-    min_learning_rate,
-    discount_factor,
-    exploration_rate,
-    exploration_decay_rate,
-    min_exploration_rate,
-    file_save_path,
-    num_bins,
-    # input_model_path="./code/models/q_table_39325_20231208_164116.pickle"
-    )
+agent = QAgent(
+	env, 
+	learning_rate,
+	learning_decay_rate,
+	min_learning_rate,
+	discount_factor,
+	exploration_rate,
+	exploration_decay_rate,
+	min_exploration_rate,
+	file_save_path,
+	num_bins,
+	# input_model_path="./code/models/q_table_39325_20231208_164116.pickle"
+	)
 
 # load pre-trained model
 # agent.load_model("./models/q_table_27873_20230713_172532.h5.npy")
